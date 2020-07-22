d6d8fd1c9f762a4ea2a6a583def9ca90eb923eb9
Szabi <szabiadam@yahoo.com>
2020-07-22 18:01:31 +0300

    background
    u

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php include "header.php"; ?>
 <?php include "db_connect.php"; ?>
 
-<h1> Hello, Welcome to the equipment database. If you want to review the equipment list, please register or login<h>
+<head>
+
+<link rel="stylesheet" type="text/css" href="css/background.css">
+
+</head
+
+<body>
+
+    <div class="bg">
+	
+		<h3 class="bg1"> Hello, Welcome to the equipment database. If you want to review the equipment list, please register or login</h3>
+	
+	</div>
+
+</body>
 
 <?php include "footer.php"; ?>
